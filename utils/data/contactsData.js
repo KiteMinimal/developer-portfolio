eea864511806777bcaf8e4d1784175434ae5b352
MOHAMMAD SHOYEB <mohdshoyeb189@gmail.com>
2025-04-25 01:24:12 +0530

    changed in contactData.js and education.js file

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,13 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'mohdshoyeb189@gmail.com',
+    phone: '+91 761 984 6221',
+    address: 'Rajgarh, Jhansi, Uttar Pradesh, India',
+    // website: 'https://said7388.github.io/portfolio',
+    github: 'https://github.com/kiteminimal',
+    facebook: 'https://www.facebook.com/mohd_shoyeb/',
+    linkedIn: 'https://www.linkedin.com/in/kiteminimal1319/',
+    twitter: 'https://twitter.com/kiteminimal',
+    stackOverflow: 'https://stackoverflow.com/users/29836568/mohammad-shoyeb',
+    devUsername: "KiteMinimal",
+    instagram: 'https://www.instagram.com/mohd_shoyeb_creator/',
 }
--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,25 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2022 - 2025",
+    institution: "Chhatrapati Shahuji Maharaj University",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Bachelor Degree",
+    duration: "2018 - 2021",
+    institution: "Bundalkhand University",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2016 - 2018",
+    institution: "St Xavier's College",
+  },
+  {
+    id: 4,
+    title: "Higher Secondary Certificate",
+    duration: "2016",
+    institution: "St Xavier's College",
   }
 ]
